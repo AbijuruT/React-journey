169b57fe6e705ede7011152dc8e8c3a1bdc214a2
Thor <abijuruyves@gmail.com>
2023-12-25 12:34:01 +0200

    Adding components

--- tic-tac-toe/src/App.jsx
+++ tic-tac-toe/src/App.jsx
@@ -1,10 +1,37 @@
+import Player from "./components/player";
 
 function App() {
   
 
   return (
-    <h1>React Tic-Tac-Toe</h1>
-  )
+		<main>
+			<div id='game-container'>
+				{/* Players */}
+				<ol id='players'>
+					<Player />
+					<Player />
+					{/* 
+          Since we are repeating ourselves it's better to use component for that repeating part
+          <li>
+						<span className='player'>
+							<span className='player-name'>Player 1</span>
+							<span className='player-symbol'>X</span>
+						</span>
+						<button>Edit</button>
+					</li>
+					<li>
+						<span className='player'>
+							<span className='player-name'>Player 2</span>
+							<span className='player-symbol'>O</span>
+						</span>
+						<button>Edit</button>
+					</li> */}
+				</ol>
+				{/* Game board */}
+			</div>
+			{/* Log */}
+		</main>
+	);
 }
 
 export default App
