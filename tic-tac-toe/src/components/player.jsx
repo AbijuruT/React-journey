bfc7f55319a8ce79fda21bab29bf58deed02df6e
Thor <abijuruyves@gmail.com>
2023-12-26 08:23:01 +0200

    Adding optimal way of conditionally changing the value of a state based on the previous value

--- tic-tac-toe/src/components/player.jsx
+++ tic-tac-toe/src/components/player.jsx
@@ -1,12 +1,22 @@
-
+import { useState } from 'react';
 export default function Player({ name, symbol }) {
-  return (
+	const [isEditing, setIsEditing] = useState(false);
+	function handleEditCLick() {
+		setIsEditing(editing => !editing);
+	}
+	let playerName = <span className='player-name'>{name}</span>;
+	// let btnCaption = 'Edit'
+	if (isEditing) {
+		playerName = <input type='text' value={name} required />;
+		// btnCaption = 'Save';
+	}
+	return (
 		<li>
 			<span className='player'>
-				<span className='player-name'>{name}</span>
+				{playerName}
 				<span className='player-symbol'>{symbol}</span>
 			</span>
-			<button>Edit</button>
+			<button onClick={handleEditCLick}>{isEditing ? 'Save' : 'Edit'}</button>
 		</li>
 	);
 }
