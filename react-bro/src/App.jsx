e90bdb400f908dda0e2e10524908b6034d17fe7c
AbijuruT <abijuruyves@gmail.com>
2024-07-04 13:58:44 +0200

    Update

--- react-bro/src/App.jsx
+++ react-bro/src/App.jsx
@@ -1,8 +1,37 @@
-import Lists from "./Lists"
+// import Lists from './Lists';
+// export default function App() {
+// 	// making a reusable lists
+
+// 	const fruits = [
+// 		{ id: 1, name: 'Apple', calories: 95 },
+// 		{ id: 2, name: 'Orange', calories: 45 },
+// 		{ id: 3, name: 'Pineapple', calories: 105 },
+// 		{ id: 4, name: 'Straw berry', calories: 159 },
+// 		{ id: 5, name: 'Coconut', calories: 37 },
+// 	];
+// 	const vegetables = [
+// 		{ id: 6, name: 'Banana', calories: 75 },
+// 		{ id: 7, name: 'Nuts', calories: 25 },
+// 		{ id: 8, name: 'Potatoes', calories: 110 },
+// 		{ id: 9, name: 'Pasta', calories: 90 },
+// 		{ id: 10, name: 'pears', calories: 107 },
+// 	];
+// 	return (
+// 		<>
+// 			{/* {fruits.length > 0 ? <Lists items={fruits} category='fruits' /> : null}
+// 			{vegetables.length > 0 ? (
+// 				<Lists items={vegetables} category='Vegetables' />
+// 			) : null} */}
+// 			{/* short hand ternary with && */}
+// 			{fruits.length > 0 && <Lists items={fruits} category='fruits' />}
+// 			{vegetables.length > 0 && <Lists items={vegetables} category='Vegetables' />}
+// 		</>
+// 	);
+// }
+//Click handling
+import Button from "./button";
 export default function App() {
 	return (
-		<>
-			<Lists/>
-		</>
-	);
+		<Button/>
+	)
 }
--- react-bro/src/Lists.jsx
+++ react-bro/src/Lists.jsx
@@ -1,34 +1,48 @@
-export default function Lists() {
-  const fruits = [
-    { id: 1,name:"Apple", calories: 95},
-    { id: 2,name: "Orange", calories: 45},
-    { id: 3,name:"Pineapple", calories:105},
-    { id: 4,name:"Straw berry", calories:159},
-    { id: 5,name:"Coconut", calories:37}
-  ];
-  // Sorting
-  fruits.sort((a, b) => a.name.localeCompare(b.name)) //Alphabetical
-  //fruits.sort((a, b) => b.name.localeCompare(a.name)); //reversed
-  // fruits.sort((a, b) => b.calories - a.calories); //numerical order
+import PropTypes from 'prop-types';
+export default function Lists(props) {
+	const category = props.category;
+	const itemList = props.items;
+	// Sorting
+	// fruits.sort((a, b) => a.name.localeCompare(b.name)) //Alphabetical
+	//fruits.sort((a, b) => b.name.localeCompare(a.name)); //reversed
+	// fruits.sort((a, b) => b.calories - a.calories); //numerical order
 
-  // const lowCalFruits = fruits.filter(fruit => fruit.calories < 100);
-  // const highCalFruits = fruits.filter((fruit) => fruit.calories >= 100);
-  // const filterdList = lowCalFruits.map((lowCalFruit) => (
+	// const lowCalFruits = fruits.filter(fruit => fruit.calories < 100);
+	// const highCalFruits = fruits.filter((fruit) => fruit.calories >= 100);
+	// const filterdList = lowCalFruits.map((lowCalFruit) => (
 	// 	<li key={lowCalFruit.id}>
 	// 		{lowCalFruit.name}: &nbsp; <b>{lowCalFruit.calories}</b>
 	// 	</li>
-  // ));
-  // const filterdList = highCalFruits.map((highCalFruit) => (
+	// ));
+	// const filterdList = highCalFruits.map((highCalFruit) => (
 	// 	<li key={highCalFruit.id}>
 	// 		{highCalFruit.name}: &nbsp; <b>{highCalFruit.calories}</b>
 	// 	</li>
 	// ));
-  
-  const lisItems = fruits.map(fruit => <li key={fruit.id}>{fruit.name}: &nbsp; <b>{ fruit.calories}</b></li>)
-  return (
-    <>
-      <ul>{lisItems }</ul>
-      {/* <ol>{filterdList }</ol> */}
-    </>
-  )
+	//Without props
+	// const lisItems = fruits.map(fruit => <li key={fruit.id}>{fruit.name}: &nbsp; <b>{ fruit.calories}</b></li>)
+	//With props
+	const lisItems = itemList.map((item) => (
+		<li key={item.id}>
+			{item.name}: &nbsp; <b>{item.calories}</b>
+		</li>
+	));
+
+	return (
+		<>
+			<h3>{category}</h3>
+			<ul>{lisItems}</ul>
+			{/* <ol>{filterdList }</ol> */}
+		</>
+	);
 }
+Lists.prototype = {
+	category: PropTypes.string,
+	items: PropTypes.arrayOf(
+		PropTypes.shape({
+			id: PropTypes.number,
+			name: PropTypes.string,
+			calories: PropTypes.number,
+		})
+	),
+};
