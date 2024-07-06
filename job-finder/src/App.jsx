94ea499111f38caf60693cf79f447fd7bc74d1f0
AbijuruT <abijuruyves@gmail.com>
2024-07-06 15:45:20 +0200

    Adding outlet

--- job-finder/src/App.jsx
+++ job-finder/src/App.jsx
@@ -4,11 +4,16 @@ import {
 	createRoutesFromElements,
 	RouterProvider,
 } from 'react-router-dom';
+import MainLayout from './layouts/MainLayout';
 import HomePage from './pages/HomePage';
 const router = createBrowserRouter(
 	//we are using index instead of path because we want to show homepage or a page
 	//For instance <Route path='/about'/ element={<p>About</p>} />
-	createRoutesFromElements(<Route index element={<HomePage />} />)
+	createRoutesFromElements(
+		<Route path='/' element={<MainLayout />}>
+			<Route index element={<HomePage />} />
+		</Route>
+	)
 );
 const App = () => {
 	return <RouterProvider router={router} />;
