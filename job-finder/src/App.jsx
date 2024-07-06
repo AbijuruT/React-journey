af6b2896a6f0a58903ba4c5e48c82d2828c9f1fa
AbijuruT <abijuruyves@gmail.com>
2024-07-06 16:18:54 +0200

    Minor change

--- job-finder/src/App.jsx
+++ job-finder/src/App.jsx
@@ -7,6 +7,7 @@ import {
 import MainLayout from './layouts/MainLayout';
 import HomePage from './pages/HomePage';
 import JobsPage from './pages/JobsPage';
+import NotFound from './pages/NotFound';
 const router = createBrowserRouter(
 	//we are using index instead of path because we want to show homepage or a page
 	//For instance <Route path='/about'/ element={<p>About</p>} />
@@ -15,6 +16,7 @@ const router = createBrowserRouter(
 		<Route path='/' element={<MainLayout />}>
 			<Route index element={<HomePage />} />
 			<Route path='/jobs' element={<JobsPage />} />
+			<Route path='*' element={<NotFound />} />
 		</Route>
 	)
 );
