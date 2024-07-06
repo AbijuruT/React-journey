fc587623b088ce3f4de5dedc1b061eb9b0327a66
AbijuruT <abijuruyves@gmail.com>
2024-07-06 16:04:30 +0200

    Adding link from react-router dom

--- job-finder/src/components/SingleJobListing.jsx
+++ job-finder/src/components/SingleJobListing.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import { FaMapMarker } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
 const SingleJobListing = ({ job }) => {
 	// Bringing in state
 	const [showFullDescription, setShowFullDescription] = useState(false); // seting the default value to false
@@ -33,11 +34,11 @@ const SingleJobListing = ({ job }) => {
 						<FaMapMarker className='inline text-lg mb-1 mr-1' />
 						{job.location}
 					</div>
-					<a
+					<Link
 						href={`/job/${job.id}`}
 						className='h-[36px] bg-indigo-500 hover:bg-indigo-600 text-white px-4 py-2 rounded-lg text-center text-sm'>
 						Read More
-					</a>
+					</Link>
 				</div>
 			</div>
 		</div>
