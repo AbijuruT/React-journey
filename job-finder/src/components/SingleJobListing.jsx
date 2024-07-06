938794ed2abb5ea3b8023e1225d336184cdf5270
AbijuruT <abijuruyves@gmail.com>
2024-07-06 13:12:42 +0200

    Adding icons

--- job-finder/src/components/SingleJobListing.jsx
+++ job-finder/src/components/SingleJobListing.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-
+import { FaMapMarker } from 'react-icons/fa';
 const SingleJobListing = ({ job }) => {
 	// Bringing in state
 	const [showFullDescription, setShowFullDescription] = useState(false);
@@ -17,6 +17,7 @@ const SingleJobListing = ({ job }) => {
 				</div>
 
 				<div className='mb-5'>{description}</div>
+				{/* Managing state */}
 				<button
 					onClick={() => setShowFullDescription((prevState) => !prevState)}
 					className='text-indigo-500 mb-4 hover:text-indigo-600'>
@@ -29,7 +30,7 @@ const SingleJobListing = ({ job }) => {
 
 				<div className='flex flex-col lg:flex-row justify-between mb-4'>
 					<div className='text-orange-700 mb-3'>
-						<i className='fa-solid fa-location-dot text-lg'></i>
+						<FaMapMarker className='inline text-lg mb-1 mr-1' />
 						{job.location}
 					</div>
 					<a
