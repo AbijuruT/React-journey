3a6bdcc4e1b8003ad8ba60d228421f563d7f1643
AbijuruT <abijuruyves@gmail.com>
2024-07-08 11:10:58 +0200

    Adding useEffect fetching

--- job-finder/src/pages/JobPage.jsx
+++ job-finder/src/pages/JobPage.jsx
@@ -1,5 +1,26 @@
+import { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import Spinner from '../components/Spinner';
 const JobPage = () => {
-	return <div></div>;
+	const { id } = useParams();
+	const [job, setJob] = useState(null);
+	const [loading, setLoading] = useState(true);
+	useEffect(() => {
+		const fetchJob = async () => {
+			try {
+				const response = await fetch(`/api/jobs/${id}`);
+				const data = await response.json();
+				setJob(data);
+			} catch (error) {
+				console.log('Error fetching data', error);
+			} finally {
+				setLoading(false);
+			}
+		};
+		fetchJob();
+	}, []);
+	return loading ? <Spinner /> : <h1>{job.title}</h1>;
 };
 
+// const jobLoader = async({params})
 export default JobPage;
