6c46c613846a4ac1542780496c1d9df08324e78c
AbijuruT <abijuruyves@gmail.com>
2024-07-08 15:15:47 +0200

    Adding add job form

--- job-finder/src/App.jsx
+++ job-finder/src/App.jsx
@@ -7,8 +7,10 @@ import {
 import MainLayout from './layouts/MainLayout';
 import HomePage from './pages/HomePage';
 import JobsPage from './pages/JobsPage';
-import JobPage, { jobLoader } from './pages/JobPage';
 import NotFound from './pages/NotFound';
+import JobPage, { jobLoader } from './pages/JobPage';
+import AddJobPage from './pages/AddJobPage';
+
 const router = createBrowserRouter(
 	//we are using index instead of path because we want to show homepage or a page
 	//For instance <Route path='/about'/ element={<p>About</p>} />
@@ -17,6 +19,7 @@ const router = createBrowserRouter(
 		<Route path='/' element={<MainLayout />}>
 			<Route index element={<HomePage />} />
 			<Route path='/jobs' element={<JobsPage />} />
+			<Route path='/add-job' element={<AddJobPage />} />
 			<Route path='/jobs/:id' element={<JobPage />} loader={jobLoader} />
 			<Route path='*' element={<NotFound />} />
 		</Route>
