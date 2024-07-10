afb2502d8ff8de92b3f751f99287e6e9a6388d09
AbijuruT <abijuruyves@gmail.com>
2024-07-10 11:49:47 +0200

    Adding new job

--- job-finder/src/App.jsx
+++ job-finder/src/App.jsx
@@ -11,21 +11,32 @@ import NotFound from './pages/NotFound';
 import JobPage, { jobLoader } from './pages/JobPage';
 import AddJobPage from './pages/AddJobPage';
 
-const router = createBrowserRouter(
-	//we are using index instead of path because we want to show homepage or a page
-	//For instance <Route path='/about'/ element={<p>About</p>} />
-	createRoutesFromElements(
-		//Creating a layout
-		<Route path='/' element={<MainLayout />}>
-			<Route index element={<HomePage />} />
-			<Route path='/jobs' element={<JobsPage />} />
-			<Route path='/add-job' element={<AddJobPage />} />
-			<Route path='/jobs/:id' element={<JobPage />} loader={jobLoader} />
-			<Route path='*' element={<NotFound />} />
-		</Route>
-	)
-);
 const App = () => {
+	//Function to add job received as a prop
+	const addJob = async (newJob) => {
+		const res = await fetch('/api/jobs', {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json',
+			},
+			body: JSON.stringify(newJob),
+		});
+		return;
+	};
+	const router = createBrowserRouter(
+		//we are using index instead of path because we want to show homepage or a page
+		//For instance <Route path='/about'/ element={<p>About</p>} />
+		createRoutesFromElements(
+			//Creating a layout
+			<Route path='/' element={<MainLayout />}>
+				<Route index element={<HomePage />} />
+				<Route path='/jobs' element={<JobsPage />} />
+				<Route path='/add-job' element={<AddJobPage addJobSubmit={addJob} />} />
+				<Route path='/jobs/:id' element={<JobPage />} loader={jobLoader} />
+				<Route path='*' element={<NotFound />} />
+			</Route>
+		)
+	);
 	return <RouterProvider router={router} />;
 };
 export default App;
